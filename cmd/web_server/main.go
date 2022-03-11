1c4178ab934574cd39bc7181f337c204e6f12071
Charlie <marmot00@gmail.com>
2022-03-12 00:50:39 +0800

    add WebSocket echo support

--- cmd/web_server/main.go
+++ cmd/web_server/main.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 type information struct {
@@ -42,6 +43,8 @@ func main() {
 	router.Handle("/exectime/{duration}", http.HandlerFunc(execTimeHandler))
 
 	router.Handle("/", http.HandlerFunc(textHandler))
+
+	router.Handle("/ws/echo", http.HandlerFunc(wsHandler))
 
 	router.Handle("/status/{code}", http.HandlerFunc(statusHandler))
 
@@ -122,6 +125,30 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 
 	_, _ = fmt.Fprint(w, string(marshal))
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
 
 // Get preferred outbound ip of this machine
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/charlie-chiu/pong
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.5.0
+)
